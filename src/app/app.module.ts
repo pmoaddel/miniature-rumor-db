e55d6f2246330d294a41779243818ee6cf15b257
Payvand Moaddel <payvand.moaddel@genesys.com>
2019-04-21 22:50:09 -0500

    initial commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,26 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { HttpClientModule } from '@angular/common/http';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { CrawlerComponent } from './crawler/crawler.component';
+import { RumorCardComponent } from './rumor-card/rumor-card.component';
+import { NavBarComponent } from './nav-bar/nav-bar.component';
+import { JumboComponent } from './jumbo/jumbo.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CrawlerComponent,
+    RumorCardComponent,
+    NavBarComponent,
+    JumboComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
